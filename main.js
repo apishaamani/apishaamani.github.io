76605d5532705335380e65ab3ba5ea6bdba28860
apishaamani <apishaa.manisekaran@torontomu.ca>
2024-02-26 19:35:23 -0500

    update

--- main.js
+++ main.js
@@ -31,20 +31,26 @@ function mOut(element) {
     some_tag.style.color = "orange";
 }
 
+
+
+
+
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
-    }
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    for (var i = 0; i < dropdowns.length; i++) {
-    var openDropdown = dropdowns[i];
-    if (openDropdown.classList.contains('show')) {
-    openDropdown.classList.remove('show');
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
     }
-    }
-    }
-    }
+  }
